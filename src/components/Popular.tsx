4c3a9c4a574cc4dd149d833b1bb69a64d5fb8511
phattharanit-pewklieng <phattharanit.noei@gmail.com>
2023-06-26 22:37:04 +1200

    update popular page

--- src/components/Popular.tsx
+++ src/components/Popular.tsx
@@ -12,21 +12,22 @@ export const Pouplar = () => {
           <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8 mb-16">
             {popularFood.map((food) => {
               return (
-                <div className="p-4 shadow-lg hover:shadow" key={food.id}>
-                  <img src={food.image} alt="" />
-                  <div className="flex items-center gap-3">
+                <div className="p-4 shadow-lg hover:shadow transition-all duration-300 cursor-pointer" key={food.id}>
+                  <img src={food.image} alt="" className='rounded-lg mb-4'/>
+                  <div className='md:text-xl text-[1rem] font-semibold'>{food.name}</div>
+                  <div className="flex items-center gap-2">
                     <button>-</button>
                     <span className="text-[0.85rem]">2</span>
                     <button>+</button>
                   </div>
-                  <p className="text-[0.85rem]">
+                  <p className="text-[0.85rem] opacity-70 mb-4">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Omnis nobis tenetur aperiam officiis nostrum voluptatem quae
                     asperiores consectetur in accusantium a maxime aliquam sint,
                     eos necessitatibus culpa ad. Eaque, voluptas.
                   </p>
-                  <div className="">
-                    <span> {food.price}</span>
+                  <div className="flex items-center justify-between">
+                    <span className='text-xl font-semibold'> {food.price}</span>
                     <span className='btn cursor-pointer p-3'>Add to cart</span>
                   </div>
                 </div>
